4bd4ca9ee8510e4758bdf48006b675b4a1efcd0a
justincage <32781073+justincage@users.noreply.github.com>
2017-11-01 13:28:55 -0500

    worked on prototype
    added rigidbody to test cube and made it shoot forward on a platform

---
+++ MechaBoxer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // created a new rigidbody for the script to access (drag and drop)
+    public Rigidbody rb;
+
+	
+	
+	// Unity likes FixedUpdate more than Update with Physics based interactions
+	void FixedUpdate ()
+    {
+        //adds a force to the rigidbody multiplied by a lesser value as time goes on
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
